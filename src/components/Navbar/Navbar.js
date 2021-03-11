a45b58b6eb403bd3e08c28f966027d842883acbe
Sachchaa <shewahal@deakin.edu.au>
2021-03-12 02:07:34 +1100

    React-sidebar-nav updated -
    Completed Navbar and NavbarElement

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,14 +2,17 @@ import React, { useState } from 'react'
 import { AiOutlineClose } from 'react-icons/ai'
 import { FaBars } from 'react-icons/fa'
 import { Data } from './Data'
+import { IconContext } from 'react-icons'
 import {
     MenuButtonContainer,
     MenuButton,
+    CloseButtonContainer,
+    CloseButton,
     Nav,
-    NavbarContainer,
     NavMenu,
     NavItem,
-    NavLink
+    NavLink,
+    Span
 
 } from './NavbarElements'
 
@@ -18,35 +21,38 @@ const Navbar = () => {
     const [sidebar, setSidebar] = useState(false)
 
     const showSideBar = () => setSidebar(!sidebar)
+
     return (
         <>
-            <MenuButtonContainer>
-                <MenuButton>
-                    <FaBars onClick={showSideBar} />
-                </MenuButton>
-            </MenuButtonContainer>
-            <Nav>
-                <NavbarContainer>
-                    <NavMenu>
-                        <NavItem>
-                            <NavLink>
-                                <AiOutlineClose />
-                            </NavLink>
-                        </NavItem>
-                        {Data.map((item, index) => {
-                            return (
-                                <NavItem key={index}>
-                                    <NavLink to={item.path}>
-                                        {item.icon} {''}
-                                        <span>{item.title}</span>
-                                    </NavLink>
-                                </NavItem>
-                            )
-                        })}
-                    </NavMenu>
-                </NavbarContainer>
-            </Nav>
+            <IconContext.Provider value={{ color: '#ffffff' }}>
+                <MenuButtonContainer>
+                    <MenuButton>
+                        <FaBars onClick={showSideBar} />
+                    </MenuButton>
+                </MenuButtonContainer>
+                {sidebar &&
+                    <Nav>
+                        <NavMenu>
+                            <CloseButtonContainer>
+                                <CloseButton>
+                                    <AiOutlineClose onClick={showSideBar} />
+                                </CloseButton>
+                            </CloseButtonContainer>
+                            {Data.map((item, index) => {
+                                return (
+                                    <NavItem key={index}>
+                                        <NavLink to={item.path}>
+                                            {item.icon} {''}
+                                            <Span>{item.title}</Span>
+                                        </NavLink>
+                                    </NavItem>
+                                )
+                            })}
+                        </NavMenu>
+                    </Nav>
+                }
 
+            </IconContext.Provider>
         </>
     )
 }
--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -2,26 +2,80 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
 export const MenuButtonContainer = styled.div`
-
+    background-color: #060b26;
+    height: 80px;
+    display: flex;
+    justify-content: start;
+    align-items: center;
 `
 export const MenuButton = styled(Link)`
+    margin-left: 2rem;
+    font-size: 2rem;
+    background: none;
+`
+
+export const Span = styled.span`
+    margin-left: 16px;
 `
 
 export const Nav = styled.nav`
+    background-color: #060b26;
+    width: 250px;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    position: fixed;
+    top:0;
+    transition: 850ms;
+
+    &:active {
+        transition: 350ms;
+    }
 
 `
 
-export const NavbarContainer = styled.div`
+export const CloseButtonContainer = styled.li`
+    background-color: #060b26;
+    width: 100%;
+    height: 80px;
+    display: flex;
+    justify-content: start;
+    align-items: center;
+`
 
+export const CloseButton = styled(Link)`
+    margin-left: 2rem;
+    font-size: 2rem;
+    background: none;
 `
 
 export const NavMenu = styled.ul`
+    width: 100%;
 
 `
 
 export const NavItem = styled.li`
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    padding: 8px 0px 8px 16px;
+    list-style: none;
+    height: 60px;
+
 `
 
 export const NavLink = styled(Link)`
+    text-decoration: none;
+    color: #f5f5f5;
+    font-size: 18px;
+    width: 95%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    padding: 0 16px;
+    border-radius: 4px;
 
+    &:hover {
+        background-color: #1a83ff;
+    }
 `
